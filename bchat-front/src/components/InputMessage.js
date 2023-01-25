ce217a93a2301340fa86142cf43d5c53f79077fb
NIGHTWING\Beria <alexander.lucas.cohort225@gmail.com>
2023-01-24 21:50:29 -0800

    [CLIENT][FIX] messages no longer send if message is only whitespace

--- bchat-front/src/components/InputMessage.js
+++ bchat-front/src/components/InputMessage.js
@@ -6,29 +6,40 @@ import SendIcon from "@mui/icons-material/Send";
 //import "./inputMessage.css";
 
 export default function InputMessage(props) {
+  //message state
   const [sendingMessage, setSendingMessage] = React.useState("");
 
+  //message state change
   const handleMessageInputChange = (event) => {
     const { value } = event.target;
     setSendingMessage(value);
   };
 
+  //checks that a message contains more than just whitespace
+  const checkWhiteSpace = (messageText) => {
+    let whitespace = /\S/g;
+    return whitespace.test(messageText);
+  };
+
   //sends message and resets input field
   const handleSendMessage = () => {
-    props.sendMessage(sendingMessage);
-    //resets input field
-    setSendingMessage("");
-    
-  }
+    //checks that message contains text
+    if (checkWhiteSpace(sendingMessage)) {
+      props.sendMessage(sendingMessage);
+      //resets input field
+      setSendingMessage("");
+    } else {
+      console.log("message did not contain text")
+    };  
+  };
 
   //checks for enter key press for submit
   const handleEnterSubmit = (event) => {
     if (event.key === 'Enter') {
       handleSendMessage();
       event.preventDefault();
-    }
-  }
-
+    };
+  };
 
 
   return (
