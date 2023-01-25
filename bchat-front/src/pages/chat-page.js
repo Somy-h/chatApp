fcc0a3e3a96f3f49522c9c961d26a7b04e248d58
NIGHTWING\Beria <alexander.lucas.cohort225@gmail.com>
2023-01-24 22:51:39 -0800

    [CLIENT][UPDATE] message stream smooth scolls to bottom with every render

--- bchat-front/src/pages/chat-page.js
+++ bchat-front/src/pages/chat-page.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
 import { UserContext } from "../context/user.context";
 import { SocketContext } from "../context/socket.context";
 
@@ -12,6 +12,19 @@ import { Box, CssBaseline } from "@mui/material";
 export default function ChatPage() {
   const { currentUser, currentChannel } = useContext(UserContext);
   const { channelMessages, sendMessage } = useContext(SocketContext);
+
+  // creates ref housed in hidden div at bottom of chat stream
+  let messageStreamEnd = useRef();
+
+  //smoothly scrolls to bottom of chat stream
+  const scrollToBottom = () => {
+    messageStreamEnd.scrollIntoView({behavior: "smooth"})
+  }
+  
+  //scrolls to bottom of message stream when page component is rendered
+  useEffect(() => {
+    scrollToBottom();
+  });
 
   const displayMessages = () =>
     channelMessages?.map((msg) => <Message key={msg.id} message={msg} />);
@@ -50,6 +63,9 @@ export default function ChatPage() {
               sx={{ flex: 1, py: 6, px: 4, bgcolor: "#eaeff1", "overflow-x": "hidden"}}
             >
               {displayMessages()}
+              <div ref={(element) => {messageStreamEnd = element;}}>
+                
+              </div>
             </Box>
             <Box component='footer' sx={{ p: 2, bgcolor: "#eaeff1" }}>
               <InputMessage sendMessage={handleSendMessage} />
